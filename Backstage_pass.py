65384c6bb0174ff7a7897525265f9d7b1d32b955
sebastia98 <sadrover@cifpfbmoll.eu>
2020-01-04 11:28:02 +0100

    fix: Cambiar orden de los simbolos en las comparaciones
    Cambiar orden de los simbolos en las comparaciones

--- Backstage_pass.py
+++ Backstage_pass.py
@@ -8,8 +8,8 @@ class Backstage_pass(Normal_item):
     def update_quality(self):
         if self.sell_in > 10:
             self.quality += 1
-        elif self.sell_in =< 10 and self.sell_in > 5:
+        elif self.sell_in <= 10 and self.sell_in > 5:
             self.quality += 2
-        elif self.sell_in =< 5 and self.sell_in > 0:
+        elif self.sell_in <= 5 and self.sell_in > 0:
             self.quality += 3
             
