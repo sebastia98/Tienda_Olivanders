acd5d7018e59bc576af58757aafb6a2d8eeb9e50
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-01-29 19:32:01 +0100

    feat: Añadir templates que dan formato a la página
    Añadir templates que dan formato a la página usando bootstrap, crear
    index como página principal y lista.html como la lista de items

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from logica import generar_html_items, gilded_rose, anadir_item
 
 app = Flask(__name__)
@@ -6,9 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
+    """
     html = generar_html_items(gilded_rose.get_items())
     html += "<a href=\"http://127.0.0.1:5000/update\">update</a>"
     return html
+    """
+    return render_template("index2.html", gilded_rose = gilded_rose)
 
 @app.route("/update")
 def update():
